bc97dd05596497c8636eec862baf17af91f2d4c1
Andres Giraldo <andresgiraldol1989@gmail.com>
2017-08-23 22:25:00 -0500

    firts commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp')
+var postcss = require('gulp-postcss')
+var cssnext = require("postcss-cssnext")
+var cssnested = require('postcss-nested')
+var mixins = require('postcss-mixins') 
+var lost = require('lost')
+var atImport = require('postcss-import')
+var csswring = require('csswring')
+var browserSync = require('browser-sync')
+
+// Servidor de desarrollo
+gulp.task('serve',function() {
+    browserSync.init({
+        server: {
+            baseDir: './dist'
+        }
+    })
+})
+
+// tarea para procesar el css
+gulp.task('css',function() {
+    var processors = [
+        atImport(),
+        mixins(),
+        cssnested,
+        lost(),
+        cssnext({ browsers: ['> 5%','ie 8'] }),
+        //csswring()
+    ];
+
+    return gulp.src('./src/invite.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream())
+})
+
+// Tarea para vigilar los cambios
+gulp.task('watch',function(){
+    gulp.watch('./src/*.css',['css'])
+    gulp.watch('./dist/*.html').on('change',browserSync.reload)    
+})
+
+gulp.task('default',['watch','serve','css'])
